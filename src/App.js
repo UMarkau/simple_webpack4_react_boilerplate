9102bd2b078c8212ceecf2e76c718177ee15fd12
Uladzimir Markau <kitanokiller@gmail.com>
2019-04-12 15:41:09 +0300

    Turn class based App component into hook.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,27 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {getCounter} from './store/reducers';
 import {counterUp, counterDown} from './store/actions';
 import styles from './App.module.scss';
 
-class App extends Component {
-    state = {
-        text: 'Hello from App!'
-    };
+const App = ({counter, counterUp, counterDown}) => {
+    const [text] = useState('Hello from the App!');
 
-    render() {
-        const {text} = this.state;
-        const {counter, counterUp, counterDown} = this.props;
-        return (
-            <>
-                <h1 className={styles.header}>{text}</h1>
-                <h3>Redux is awesome</h3>
-                <h5>{counter}</h5>
-                <div className={styles.row}>
-                    <button className={`${styles.btnColor} ${styles.btnExtraWidth}`} onClick={counterUp}>
-                        Counter UP
-                    </button>
-                    <button onClick={counterDown}>Counter DOWN</button>
-                </div>
-            </>
-        );
-    }
-}
+    return (
+        <>
+            <h1 className={styles.header}>{text}</h1>
+            <h3>Redux is awesome</h3>
+            <h5>{counter}</h5>
+            <div className={styles.row}>
+                <button className={`${styles.btnColor} ${styles.btnExtraWidth}`} onClick={counterUp}>
+                    Counter UP
+                </button>
+                <button onClick={counterDown}>Counter DOWN</button>
+            </div>
+        </>
+    );
+};
 
 App.propTypes = {
     counter: PropTypes.number.isRequired,
